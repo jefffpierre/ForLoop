8dfbec67709f4a83d793e47a49864d84a0710041
Jeff Pierre <49200624+jefffpierre@users.noreply.github.com>
2022-02-21 22:31:02 +0000

    Adding Range Function, commented out previous ForLoop using CTRL K followed by CTRL C

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-fruits = ["Apple", "Peach", "Pear"]
+# fruits = ["Apple", "Peach", "Pear"]
 
-for fruit in fruits:
-  print(fruit + " Pie")
+# for fruit in fruits:
+#   print(fruit + " Pie")
+
+# print()
+
+#For Loop with Range
+for number in range(1,10):
+  
