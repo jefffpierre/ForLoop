2dd4773f141e597ce6d11f6de89cfd5d997d3ccd
Jeff Pierre <49200624+jefffpierre@users.noreply.github.com>
2022-02-21 22:40:10 +0000

    added For/Range that'll produce the sum of 1 to 100

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ print()
 #For Loop with Range
 for number in range(1,10, 3): #adding the last number at the end allows us to skip through the range
   print(number)
+
+#This For/Range will produce the sum of 1 to 100
+total = 0 
+for number in range(1,101):
+    total += number
+print(total)
