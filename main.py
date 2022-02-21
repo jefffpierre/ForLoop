1ac5cb991017634985bcd8cd03f6faa62815eda7
Jeff Pierre <49200624+jefffpierre@users.noreply.github.com>
2022-02-21 22:35:03 +0000

    added a third number that skips/hops through range

--- main.py
+++ main.py
@@ -6,5 +6,11 @@
 # print()
 
 #For Loop with Range
-for number in range(1,10):
-  
+for number in range(1,10): #last number in the range won't be included in the list
+  print(number)
+
+print()
+
+#For Loop with Range
+for number in range(1,10, 3): #adding the last number at the end allows us to skip through the range
+  print(number)
