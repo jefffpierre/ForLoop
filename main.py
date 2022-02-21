e3fdaa6623b8df43c9d8967e2cb8e40f3c886e3a
Jeff Pierre <49200624+jefffpierre@users.noreply.github.com>
2022-02-21 22:49:48 +0000

    Finding the Sum of Even Numbers 1 - 100 (OPTION 2)

--- main.py
+++ main.py
@@ -26,3 +26,10 @@ total = 0
 for number in range(0,101, 2):
     total += number
 print(total)
+
+#Finding the Sum of Even Numbers 1 - 100 (OPTION 2)
+total2 = 0
+for number in range(1,101):
+  if number % 2 == 0:
+    total2 += number
+print(total2)
